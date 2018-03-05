64ca034258de8471787a60cb1a9edfe4bd8b2893
Marlon <mqchado@gmail.com>
2018-03-05 16:47:35 -0500

    segundo sout

--- src/javaapplication21/JavaApplication21.java
+++ src/javaapplication21/JavaApplication21.java
@@ -16,6 +16,7 @@ public class JavaApplication21 {
      */
     public static void main(String[] args) {
         System.out.println(" empiezo sout ");
+         System.out.println(" otro  sout ");
     }
     
 }
